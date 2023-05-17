6879748d1a951de562ffa1c3e61618af9c1bc00c
kushal-dreamspring <kkhare@dreamspring.org>
2023-05-17 09:24:19 +0530

    Update production.rb config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
@@ -63,6 +63,22 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "MediApp_production"
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'kushalkhare.official@gmail.com',
+    password: 'zfitcqbugkkxhdnl',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    open_timeout: 5,
+    read_timeout: 5
+  }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
