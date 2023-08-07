516d2e28895927ee73360f1ce3716fc2cbdc5ba1
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-07 13:49:25 +0530

    Validate date_time in Appointment model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -7,4 +7,13 @@ class Appointment < ApplicationRecord
 
   belongs_to :doctor
   belongs_to :user
+  validate :date_must_be_in_future
+
+  private
+
+  def date_must_be_in_future
+    return if date_time >= DateTime.current
+
+    errors.add(:date_time, 'must be in future')
+  end
 end
