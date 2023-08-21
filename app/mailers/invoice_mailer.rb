a3dae63e9d7449ce60138f3376c6b0fef11bae77
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-21 10:28:37 +0530

    Refactor Invoice Mailer
    - Use AppointmentsHelper in InvoiceMailer
    - Remove url param from InvoiceMailer
    - Refactor mailer specs

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -1,12 +1,13 @@
 class InvoiceMailer < ApplicationMailer
+  helper :appointments
+
   default from: 'kushalkhare.official@gmail.com'
 
   def invoice_email
     @appointment = Appointment.find_by(id: params[:appointment_id])
-    @url = params[:url]
 
     return unless @appointment
 
-    mail(to: @appointment.user.email, subject: 'MediApp: Thanks for booking an appointment with us')
+    mail(to: @appointment.user.email, subject: I18n.t('medi_app_thanks_for_booking_an_appointment_with_us'))
   end
 end
--- spec/mailers/invoice_spec.rb
+++ spec/mailers/invoice_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.describe InvoiceMailer, type: :mailer do
   fixtures :appointments
 
-  let(:mail) { InvoiceMailer.with(appointment_id: appointments(:one).id, url: 'https://mediapp.com/appointments/1').invoice_email }
+  let(:mail) { InvoiceMailer.with(appointment_id: appointments(:one).id).invoice_email }
 
   it 'renders the headers' do
     expect(mail.subject).to eq('MediApp: Thanks for booking an appointment with us')
     expect(mail.to).to eq(['johndoe@test.com'])
     expect(mail.from).to eq(['kushalkhare.official@gmail.com'])
   end
-
-  # it "renders the body" do
-  #   expect(mail.body.encoded).to match("Hi")
-  # end
 end
