23a823bf0a3ee9e3832b56113d7014724e82152b
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-21 10:32:56 +0530

    Minor Refactors
    - Internationalize loader text
    - Indent application.html.erb and application.pdf.erb
    - Update en locale
    - Remove loop from doctors#index view
    - Remove loop from appointments#index view
    - Update appointments view specs
    -

--- spec/views/appointments/index.html.erb_spec.rb
+++ spec/views/appointments/index.html.erb_spec.rb
@@ -5,17 +5,21 @@ RSpec.describe "appointments/index", type: :view do
 
   before(:each) do
     assign(:appointments, [
-      Appointment.create!(
-        doctor: doctors(:one),
-        user: users(:zero),
-        date_time: DateTime.now + 1.hour
-      ),
-      Appointment.create!(
-        doctor: doctors(:two),
-        user: users(:zero),
-        date_time: DateTime.now + 2.hour
-      )
-    ])
+             Appointment.create!(
+               doctor: doctors(:one),
+               user: users(:one),
+               date_time: DateTime.now + 1.hour,
+               amount: 500,
+               conversion_rates: { "EUR": 0.011076, "INR": 1, "USD": 0.012029 }
+             ),
+             Appointment.create!(
+               doctor: doctors(:two),
+               user: users(:one),
+               date_time: DateTime.now + 2.hour,
+               amount: 500,
+               conversion_rates: { "EUR": 0.011076, "INR": 1, "USD": 0.012029 }
+             )
+           ])
   end
 
   it "renders a list of appointments" do
