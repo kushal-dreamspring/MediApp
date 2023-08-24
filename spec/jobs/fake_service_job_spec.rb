fc2d8701ed2e1b6d81c691097e10670e0afa7672
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-24 14:54:32 +0530

    check params in fake_service_job_spec.rb

--- spec/jobs/fake_service_job_spec.rb
+++ spec/jobs/fake_service_job_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe FakeServiceJob, type: :job do
   fixtures :appointments
 
   it 'should broadcast using turbo_stream' do
-    expect(Turbo::StreamsChannel).to receive(:broadcast_render_later_to)
+    expect(Turbo::StreamsChannel).to receive(:broadcast_render_later_to).with(:appointment_created,
+                                                                              target: 'new_appointment',
+                                                                              partial: 'appointments/appointment_success',
+                                                                              locals: { appointment_time: appointments(:one).date_time })
     FakeServiceJob.perform_now(appointments(:one))
   end
 end
