0f57d29d35d8c7bd41818a0aa3ac16f71c128ea7
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-24 15:08:12 +0530

    Refactor invoice_mailer_spec.rb
    - Add url and doctor name specs for mail body

--- spec/mailers/invoice_mailer_spec.rb
+++ spec/mailers/invoice_mailer_spec.rb
@@ -6,8 +6,18 @@ RSpec.describe InvoiceMailer, type: :mailer do
   let(:mail) { InvoiceMailer.with(appointment_id: appointments(:one).id).invoice_email }
 
   it 'renders the headers' do
-    expect(mail.subject).to eq(I18n.t('medi_app_thanks_for_booking_an_appointment_with_us'))
+    expect(mail.subject).to eq(I18n.t('invoice_mail.subject'))
     expect(mail.to).to include(appointments(:one).user.email)
     expect(mail.from).to include(InvoiceMailer::DEFAULT_SENDER_MAIL)
+  end
+
+  describe 'render the body' do
+    it 'should contain the name of the doctor' do
+      expect(mail.body.encoded).to include("Dr. #{appointments(:one).doctor.name}")
+    end
+
+    it 'should contain the link to the app' do
+      expect(mail.body.encoded).to include("#{Rails.application.routes.default_url_options[:host]}/appointments/")
+    end
   end
 end
