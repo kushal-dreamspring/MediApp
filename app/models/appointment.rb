d8a8b161481379d0f9b32472abf315f4b5c8df4a
kushal-dreamspring <kkhare@dreamspring.org>
2023-08-24 14:55:25 +0530

    Refactor locales in appointment model error messages

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,8 +5,8 @@ class Appointment < ApplicationRecord
 
   validates_presence_of :doctor, :user, :date_time, :amount, :conversion_rates
   validates :date_time, comparison: { greater_than: DateTime.current }
-  validates_uniqueness_of :date_time, scope: [:user], message: I18n.t('user_already_has_a_appointment_at_this_time')
-  validates_uniqueness_of :date_time, scope: [:doctor], message: I18n.t('doctor_already_has_a_appointment_at_this_time')
+  validates_uniqueness_of :date_time, scope: [:user], message: I18n.t('appointments_model.error_messages.user_already_has_a_appointment_at_this_time')
+  validates_uniqueness_of :date_time, scope: [:doctor], message: I18n.t('appointments_model.error_messages.doctor_already_has_a_appointment_at_this_time')
 
   def amount_in_preferred_currency
     (amount * conversion_rates[user.currency_preference]).round(2)
